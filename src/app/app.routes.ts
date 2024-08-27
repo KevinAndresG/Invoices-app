360a6319ab393756b4f7c44e386f44e92dc67509
KevinAndresG <kevinandresg@gmail.com>
2024-08-27 09:41:21 -0500

    Create Routing to manage the invoices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard/invoices-list' },
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    redirectTo: 'dashboard/invoices-list',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./Pages/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    children: [
+      {
+        path: 'invoices-list',
+        loadComponent: () =>
+          import('./Pages/invoices-list/invoices-list.component').then(
+            (m) => m.InvoicesListComponent
+          ),
+      },
+      {
+        path: 'invoice/:id',
+        loadComponent: () =>
+          import('./Pages/invoice-details/invoice-details.component').then(
+            (m) => m.InvoiceDetailsComponent
+          ),
+      },
+      {
+        path: 'create-invoice',
+        loadComponent: () =>
+          import('./Pages/create-invoice/create-invoice.component').then(
+            (m) => m.CreateInvoiceComponent
+          ),
+      },
+      {
+        path: 'edit-invoice/:id',
+        loadComponent: () =>
+          import('./Pages/create-invoice/create-invoice.component').then(
+            (m) => m.CreateInvoiceComponent
+          ),
+      },
+    ],
+  },
+];
